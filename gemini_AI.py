ddcc259c3e0e1d177bbf42b9ea1cf10678f7847a
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-03 18:30:24 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -23,7 +23,7 @@ Follow these simple steps to interact with the chatbot:
 3. Ensure your **prompt is clear and complete** with some context for better result.
 """)
 
-st.markdown("![Alt Text](https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjl2dGNiYThobHplMG81aGNqMjdsbWwwYWJmbTBncGp6dHFtZTFzMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CGP9713UVzQ0BQPhSf/giphy.gif) =50x50")
+st.markdown("![Alt Text](https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjl2dGNiYThobHplMG81aGNqMjdsbWwwYWJmbTBncGp6dHFtZTFzMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CGP9713UVzQ0BQPhSf/giphy.gif) Height = "50"")
        
 # This is the first API key input; no need to repeat it in the main function.
 #api_key = st.text_input("Enter your Google API Key:", type="password", key="api_key_input")
