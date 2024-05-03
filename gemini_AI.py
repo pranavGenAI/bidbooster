deda940dda7622a6a4998af8651f8746bed0320d
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-03 13:57:37 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -53,7 +53,7 @@ def get_conversational_chain():
     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n. And remember to format your answer in nicer way.
     Do not copy and paste the context. Summarize it in better way and then provide the answer. 
     Context:\n {context}?\n
-    Question: \n{question}\n
+    Question: \n{question}\n .Provide summarize answer in easy to understand language and format it in better way.
 
     Answer:
     """
