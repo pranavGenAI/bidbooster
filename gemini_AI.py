6f95ae530b157aa92c44854b1661a2fc1f76b78d
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-03 19:46:53 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -1,97 +1,98 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-#from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-import os
-from langchain_community.vectorstores import FAISS
+# import streamlit as st
+# from PyPDF2 import PdfReader
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# import google.generativeai as genai
+# #from langchain.vectorstores import FAISS
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain.chains.question_answering import load_qa_chain
+# from langchain.prompts import PromptTemplate
+# import os
+# from langchain_community.vectorstores import FAISS
 
-st.set_page_config(page_title="Document Genie", layout="wide")
+# st.set_page_config(page_title="Document Genie", layout="wide")
 
-st.markdown("""
-## BidBooster 🤗💬: Answers your RFP related query.
+# st.markdown("""
+# ## BidBooster 🤗💬: Answers your RFP related query.
 
-### How It Works?
+# ### How It Works?
 
-Follow these simple steps to interact with the chatbot:
-1. **Upload the RFP document and click on submit & process** (Please note: the base LLM model is fine-tuned on LCBO ESG RFP documents. Results may vary for other documents).
-2. **Ask a Question:** Once the document is processed, ask any question related to its content for a precise answer.
-3. Ensure your **prompt is clear and complete** with some context for better result.
-""")
+# Follow these simple steps to interact with the chatbot:
+# 1. **Upload the RFP document and click on submit & process** (Please note: the base LLM model is fine-tuned on LCBO ESG RFP documents. Results may vary for other documents).
+# 2. **Ask a Question:** Once the document is processed, ask any question related to its content for a precise answer.
+# 3. Ensure your **prompt is clear and complete** with some context for better result.
+# """)
 
-st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjl2dGNiYThobHplMG81aGNqMjdsbWwwYWJmbTBncGp6dHFtZTFzMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CGP9713UVzQ0BQPhSf/giphy.gif", width=50)
+# st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjl2dGNiYThobHplMG81aGNqMjdsbWwwYWJmbTBncGp6dHFtZTFzMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CGP9713UVzQ0BQPhSf/giphy.gif", width=50)
 
-# This is the first API key input; no need to repeat it in the main function.
-#api_key = st.text_input("Enter your Google API Key:", type="password", key="api_key_input")
+# # This is the first API key input; no need to repeat it in the main function.
+# api_key = st.text_input("Enter your Google API Key:", type="password", key="api_key_input")
 
+st.image("https://media1.tenor.com/m/6o864GYN6wUAAAAC/interruption-sorry.gif", width=1000)
 
-api_key = st.secrets['GEMINI_API_KEY']
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+# #api_key = st.secrets['GEMINI_API_KEY']
+# def get_pdf_text(pdf_docs):
+#     text = ""
+#     for pdf in pdf_docs:
+#         pdf_reader = PdfReader(pdf)
+#         for page in pdf_reader.pages:
+#             text += page.extract_text()
+#     return text
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+# def get_text_chunks(text):
+#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+#     chunks = text_splitter.split_text(text)
+#     return chunks
 
-def get_vector_store(text_chunks, api_key):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+# def get_vector_store(text_chunks, api_key):
+#     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+#     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+#     vector_store.save_local("faiss_index")
 
-def get_conversational_chain():
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context and in polite way, make sure to provide all the details in summarized format, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n. 
-    Do not copy and paste the context. Summarize it in better way and then provide the answer. 
-    Context:\n {context}?\n
-    Question: \n{question}\n .Provide the short summary paragraph of complete answer at the begining and then provide the rest of the answer transformed into easy to understand langauge. Limit the detailed answer and reword each point into more easy language.
+# def get_conversational_chain():
+#     prompt_template = """
+#     Answer the question as detailed as possible from the provided context and in polite way, make sure to provide all the details in summarized format, if the answer is not in
+#     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n. 
+#     Do not copy and paste the context. Summarize it in better way and then provide the answer. 
+#     Context:\n {context}?\n
+#     Question: \n{question}\n .Provide the short summary paragraph of complete answer at the begining and then provide the rest of the answer transformed into easy to understand langauge. Limit the detailed answer and reword each point into more easy language.
 
-    Answer:
-    """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-    return chain
+#     Answer:
+#     """
+#     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
+#     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+#     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+#     return chain
 
-def user_input(user_question, api_key):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
-    #new_db = FAISS.load_local("faiss_index", embeddings)
-    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
-    chain = get_conversational_chain()
-    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
-    st.write("Reply: ", response["output_text"])
+# def user_input(user_question, api_key):
+#     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+#     #new_db = FAISS.load_local("faiss_index", embeddings)
+#     new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
+#     docs = new_db.similarity_search(user_question)
+#     chain = get_conversational_chain()
+#     response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+#     st.write("Reply: ", response["output_text"])
 
-def main():
-    st.header("BidBooster Chatbot")
+# def main():
+#     st.header("BidBooster Chatbot")
 
-    user_question = st.text_input("Ask a Question from the PDF Files", key="user_question")
+#     user_question = st.text_input("Ask a Question from the PDF Files", key="user_question")
 
-    if user_question and api_key:  # Ensure API key and user question are provided
-        user_input(user_question, api_key)
+#     if user_question and api_key:  # Ensure API key and user question are provided
+#         user_input(user_question, api_key)
 
-    with st.sidebar:    
+#     with st.sidebar:    
         
-        st.title("BidBooster 🤗💬")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
-        if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks, api_key)
-                st.success("Done")
-        st.image("https://media.tenor.com/s1Y9XfdN08EAAAAi/bot.gif", width=200)
+#         st.title("BidBooster 🤗💬")
+#         pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
+#         if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
+#             with st.spinner("Processing..."):
+#                 raw_text = get_pdf_text(pdf_docs)
+#                 text_chunks = get_text_chunks(raw_text)
+#                 get_vector_store(text_chunks, api_key)
+#                 st.success("Done")
+#         st.image("https://media.tenor.com/s1Y9XfdN08EAAAAi/bot.gif", width=200)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
