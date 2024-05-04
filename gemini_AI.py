107543ebdfe0a5b223dce10492b4c3697bd94d18
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-04 17:35:31 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
@@ -10,25 +11,32 @@ from langchain.prompts import PromptTemplate
 import os
 from langchain_community.vectorstores import FAISS
 
-st.set_page_config(page_title="BidBooster Query Bot", layout="wide")
+st.set_page_config(page_title="Document Genie", layout="wide")
 
-#st.markdown("""
+st.markdown("""
 ## BidBooster 🤗💬: Answers your RFP related query.
 
 ### How It Works?
 
-#Follow these simple steps to interact with the chatbot:
-#1. **Upload the RFP document and click on submit & process** (Please note: the base LLM model is fine-tuned on LCBO ESG RFP documents. Results may vary for other documents).
-#2. **Ask a Question:** Once the document is processed, ask any question related to its content for a precise answer.
-#3. Ensure your **prompt is clear and complete** with some context for better result.
-#""")
-
-#st.image("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjl2dGNiYThobHplMG81aGNqMjdsbWwwYWJmbTBncGp6dHFtZTFzMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CGP9713UVzQ0BQPhSf/giphy.gif", width=50)
+Follow these simple steps to interact with the chatbot:
+1. **Upload the RFP document and click on submit & process** (Please note: the base LLM model is fine-tuned on LCBO ESG RFP documents. Results may vary for other documents).
+2. **Ask a Question:** Once the document is processed, ask any question related to its content for a precise answer.
+3. Ensure your **prompt is clear and complete** with some context for better result.
+""")
+file_ = open("C:/Users/pranav.baviskar/Desktop/Learning/GenAI/BidBooster/giphy.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+#data_url = "C:/Users/pranav.baviskar/Desktop/Learning/GenAI/BidBooster/giphy.gif"
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="arrow gif" width="50" height="50">',
+    unsafe_allow_html=True,
+)
 
 # This is the first API key input; no need to repeat it in the main function.
-st.image("https://media1.tenor.com/m/6o864GYN6wUAAAAC/interruption-sorry.gif", width=1000)
+#api_key = st.text_input("Enter your Google API Key:", type="password", key="api_key_input")
 
-#api_key = st.secrets['GEMINI_API_KEY']
+api_key = 'AIzaSyAJT6_IYPjUtUyT14uzZ8BSON7rDul7Ab8'
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -50,15 +58,16 @@ def get_vector_store(text_chunks, api_key):
 def get_conversational_chain():
     prompt_template = """
     Answer the question as detailed as possible from the provided context and in polite way, make sure to provide all the details in summarized format, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n. 
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n. And remember to format your answer in nicer way.
     Do not copy and paste the context. Summarize it in better way and then provide the answer. 
     Context:\n {context}?\n
-    Question: \n{question}\n .Provide the short summary paragraph with easy to comprehend language of complete answer at the begining and then provide the detailed answer. Detailed answer should be crisp and reword each point into more easy language.
+    Question: \n{question}\n .Provide summarize answer in easy to understand language and format it in better way. Do not copy and paste the context but summarize it and change wording.
 
-     Answer:
+    Answer:
     """
     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    print("Prompt ***** --->", prompt)
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
 
@@ -73,19 +82,32 @@ def user_input(user_question, api_key):
 
 def main():
     st.header("BidBooster Chatbot")
+
     user_question = st.text_input("Ask a Question from the PDF Files", key="user_question")
+
     if user_question and api_key:  # Ensure API key and user question are provided
         user_input(user_question, api_key)
-        with st.sidebar:    
-            st.title("BidBooster 🤗💬")
-            pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
-            if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
-                with st.spinner("Processing..."):
-                    raw_text = get_pdf_text(pdf_docs)
-                    text_chunks = get_text_chunks(raw_text)
-                    get_vector_store(text_chunks, api_key)
-                    st.success("Done")
-            st.image("https://media.tenor.com/s1Y9XfdN08EAAAAi/bot.gif", width=200)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
+        if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks, api_key)
+                st.success("Done")
+
+        file_ = open("C:/Users/pranav.baviskar/Desktop/Learning/GenAI/BidBooster/bot.gif", "rb")
+        contents = file_.read()
+        data_url = base64.b64encode(contents).decode("utf-8")
+        file_.close()
+        #data_url = "C:/Users/pranav.baviskar/Desktop/Learning/GenAI/BidBooster/giphy.gif"
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="arrow gif" width="200" height="200">',
+            unsafe_allow_html=True,
+        )
+
 
 if __name__ == "__main__":
     main()
