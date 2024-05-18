f484c9aaf8c59622b59f5f07598388ed4b393f77
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-18 11:19:52 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
     }
     .stApp {
         background: linear-gradient(45deg, #0a1621 20%, #0E1117 45%, #0E1117 55%, #3a5683 90%);
-        animation: my_animation 10s ease infinite;
+        animation: my_animation 20s ease infinite;
         background-size: 200% 200%;
         background-attachment: fixed;
     }
