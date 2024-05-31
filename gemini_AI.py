b523fe20833b5b7875c42ee88a229542c28e9791
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-31 22:05:57 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -23,7 +23,7 @@ video_html = """
 		  bottom: 0;
 		  min-width: 100%; 
 		  min-height: 100%;
-		  filter: brightness(40%); /* Adjust the brightness to make the video darker */
+		  filter: brightness(20%); /* Adjust the brightness to make the video darker */
 		}
 		
 		.content {
