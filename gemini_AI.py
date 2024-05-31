bf5083c44d4598f9e792924ce8bcd0c858c47027
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-31 22:00:08 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -15,6 +15,34 @@ import time
 
 st.set_page_config(page_title="BidBooster ", layout="wide")
 
+video_html = """
+		<style>
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		  filter: brightness(30%); /* Adjust the brightness to make the video darker */
+		}
+		
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5); /* Adjust the transparency as needed */
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
+		</style>	
+		<video autoplay muted loop id="myVideo">
+		  <source src="https://assets.mixkit.co/videos/47285/47285-720.mp4" type="video/mp4">
+		  Your browser does not support HTML5 video.
+		</video>
+		"""
+
+st.markdown(video_html, unsafe_allow_html=True)
+
 st.markdown("""
     <style>
         @keyframes gradientAnimation {
