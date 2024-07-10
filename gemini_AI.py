08410d766cc3c44c8de51a3eebe706e51b83500f
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-10 18:30:41 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -126,7 +126,7 @@ users = {
 
 def login():
     st.title("Login")
-    col1,col2,col3 = [1,1,1]
+    col1, col2, col3 = st.columns([1, 1, 1])
     with col1:
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
