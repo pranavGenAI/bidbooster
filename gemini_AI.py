1ed7ba38bac53beb3e1c38dd8e964dbb4a73b359
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-10 18:34:54 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -132,7 +132,7 @@ def login():
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
 	    
-        if st.button("Login"):
+        if st.button("Sign in"):
             hashed_password = hash_password(password)
             if username in users and users[username] == hashed_password:
                 st.session_state.logged_in = True
