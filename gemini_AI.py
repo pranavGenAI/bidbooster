17ef1c42f48f0b939b3ff9df5711d0850b5032a1
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-11 20:49:17 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -178,7 +178,7 @@ def login():
     col4, col5, col6, col7, col8, col9 = st.columns([1, 1, 1, 1, 1, 1])
     st.markdown("")
     st.markdown("")
-    with col4:
+    with col7:
         st.markdown("")
         st.markdown("")
         st.markdown("")
