a20e935621c40f52669a263ef6901b4cdae680eb
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-02-12 08:54:50 +0530

    gemini_AI.py

--- gemini_AI.py
+++ gemini_AI.py
@@ -17,86 +17,6 @@ import json
 
 st.set_page_config(page_title="Bid Query Bot ", layout="wide")
 
-# video_html = """
-# 		<style>
-# 		#myVideo {
-# 		  position: fixed;
-# 		  right: 0;
-# 		  bottom: 0;
-# 		  min-width: 100%; 
-# 		  min-height: 100%;
-# 		  filter: brightness(25%); /* Adjust the brightness to make the video darker */
-# 		}
-		
-# 		.content {
-# 		  position: fixed;
-# 		  bottom: 0;
-# 		  background: rgba(0, 0, 0, 0.2); /* Adjust the transparency as needed */
-# 		  color: #f1f1f1;
-# 		  width: 100%;
-# 		  padding: 20px;
-# 		}
-# 		</style>	
-# 		<video autoplay muted loop id="myVideo">
-# 		  <source src="https://assets.mixkit.co/videos/4907/4907-720.mp4" type="video/mp4">
-# 		  Your browser does not support HTML5 video.
-# 		</video>
-# 		"""
-
-# st.markdown(video_html, unsafe_allow_html=True)
-
-# st.markdown("""
-#     <style>
-#         @keyframes gradientAnimation {
-#             0% {
-#                 background-position: 0% 50%;
-#             }
-#             50% {
-#                 background-position: 100% 50%;
-#             }
-#             100% {
-#                 background-position: 0% 50%;
-#             }
-#         }
-
-#         .animated-gradient-text_ {
-#             font-family: "Graphik Semibold";
-#             font-size: 42px;
-#             background: linear-gradient(45deg, rgb(245, 58, 126) 30%, rgb(200, 1, 200) 55%, rgb(197, 45, 243) 20%);
-#             background-size: 300% 200%;
-#             -webkit-background-clip: text;
-#             -webkit-text-fill-color: transparent;
-#             animation: gradientAnimation 10s ease-in-out infinite;
-#         }
-#         @keyframes animate_ {
-#             0%, 18%, 20%, 50.1%,60%, 65.1%, 80%,90.1%,92% {
-#                 color: #0e3742;
-#                 text-shadow: none;
-#                 }
-#             18.1%, 20.1%, 30%,50%,60.1%,65%,80.1%,90%, 92.1%,100% {
-#                 color: #fff;
-#                 text-shadow: 0 0 10px rgb(197, 45, 243),
-#                              0 0 20px rgb(197, 45, 243);
-#                 }
-#             }
-        
-#         .animated-gradient-text_ {
-#                     font-family: "Graphik Semibold";
-#                     font-size: 42px;
-#                     color: #FFF;
-#                     transition: color 0.5s, text-shadow 0.5s;
-#                 }
-
-#         .animated-gradient-text_:hover {
-#                     animation: animate_ 5s linear infinite;
-#                 }
-
-        
-#     </style>
-#     <p class="animated-gradient-text_">
-#         Bid Query Bot: Simplifying Your Bid Process!
-#     </p>
-# """, unsafe_allow_html=True)
 st.markdown(
     """
     <style>
@@ -203,22 +123,6 @@ def update_token_count(username, count):
     token_counts = read_token_counts()
     token_counts[username] = count
     write_token_counts(token_counts)
-
-
-# st.markdown("""
-#     <style>
-#         /* Make the label background transparent */
-#         label[for="user_question"] {
-#             background-color: transparent !important;
-#         }
-
-#         /* Change the input box background to white */
-#         div[data-baseweb="input"] > div {
-#             background-color: white !important;
-#             border-radius: 5px !important;
-#         }
-#     </style>
-# """, unsafe_allow_html=True)
 
 def login():
     col1, col2, col3 = st.columns([1, 1, 1])  # Create three columns with equal width
@@ -389,7 +293,7 @@ def main():
     </style>
     """, unsafe_allow_html=True)
 
-    user_question = st.text_input(key="user_question")
+    user_question = st.text_input("How can I help you?", key="user_question")
 
     if user_question and api_key:  # Ensure API key and user question are provided
         if user_question:
@@ -410,45 +314,7 @@ def main():
                 st.success("Done")
 
 if __name__ == "__main__":
-    # with open('https://github.com/pranavGenAI/bidbooster/blob/475ae18b3c1f5a05a45ff983e06b025943137576/wave.css') as f:
-        # css = f.read()
 
-    # st.markdown('''<style>
-    #     .stApp > header {
-    #     background-color: transparent;
-    # }
-    # .stApp {
-    #     background: linear-gradient(45deg, #0a1621 20%, #0E1117 45%, #0E1117 55%, #3a5683 90%);
-    #     animation: my_animation 20s ease infinite;
-    #     background-size: 200% 200%;
-    #     background-attachment: fixed;
-    # }
-    # @keyframes my_animation {
-    #     0% {background-position: 0% 0%;}
-    #     50% {background-position: 100% 100%;}
-    #     100% {background-position: 0% 0%;}
-    # }
-    # [data-testid=stSidebar] {
-    #     background: linear-gradient(360deg, #1a2631 95%, #161d29 10%);
-    # }
-    # div.stButton > button:first-child {
-    #     background:linear-gradient(45deg, #c9024b 45%, #ba0158 55%, #cd006d 70%);
-    #     color: white;
-    #     border: none;
-    # }
-    # div.stButton > button:hover {
-    #     background:linear-gradient(45deg, #ce026f 45%, #970e79 55%, #6c028d 70%);
-    #     background-color:#ce1126;
-    # }
-    # div.stButton > button:active {
-    #     position:relative;
-    #     top:3px;
-    # }    
-
-
-    # </style>''', unsafe_allow_html=True)
-
-    # Ensure session state variables are initialized
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
     if "tokens_consumed" not in st.session_state:
@@ -472,15 +338,15 @@ st.markdown(
     @import url('https://fonts.googleapis.com/css2?family=Graphik:wght@400;700&display=swap');
 
     body {
-        background-color: #f0f0f0;
+        background-color: #000000;
         color: black;
         font-family: 'Graphik', sans-serif;
     }
     .main {
-        background-color: #f0f0f0;
+        background-color: #000000;
     }
     .stApp {
-        background-color: #f0f0f0;
+        background-color: #000000;
     }
     header {
         background-color: #660094 !important;
